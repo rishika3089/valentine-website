cb9328d872c99d35f8ac8c4539be89717a481332
Rishika Rajput <rishrajput05@gmail.com>
2026-02-14 19:26:30 +0530

    Configure Flask app to run on host and port
    Updated app.run to specify host and port for deployment.

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# IMPORTANT for Render
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
